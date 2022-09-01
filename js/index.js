fd9e39a6e66cf435343161725254436919f09a5d
Samir <sakitov@yandex.ru>
2022-09-01 16:26:38 +0400

    Сделал полностью к сдаче страницу Toys

--- js/index.js
+++ js/index.js
@@ -2,19 +2,19 @@ const popup = document.querySelector('.popup');
 const popupClose = popup.querySelector('.popup__close');
 const popupOpen = document.querySelector('.slider__info__btn');
 const checkboxAgreement = popup.querySelector('#agreement');
-const submitPopup = popup.querySelector('.feedback-form__submit')
+const submitPopup = popup.querySelector('.feedback-form__submit');
 
 const POPUP_ACTIVE = 'popup--active';
 
-// закрытие и открытие попапа
 function closePopup() {
   popup.classList.remove(POPUP_ACTIVE);
 }
 
-function switchDisabled () {
-    submitPopup.disabled = !submitPopup.disabled
+function switchDisabled() {
+  submitPopup.disabled = !submitPopup.disabled;
 }
 
+// закрытие и открытие попапа
 if (popupOpen) {
   popupOpen.addEventListener('click', () => {
     if (popup) {
@@ -40,6 +40,11 @@ if (popupClose) {
   });
 }
 
-// активировать кнопку формы
-checkboxAgreement.addEventListener('click', switchDisabled)
+popup.addEventListener('click', function (event) {
+  if (event.target === popup) {
+    closePopup();
+  }
+});
 
+// активировать кнопку формы
+checkboxAgreement.addEventListener('click', switchDisabled);
